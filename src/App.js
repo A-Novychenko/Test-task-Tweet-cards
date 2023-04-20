20a5a8690d79154b6a7617774147ecfe92a7c213
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-20 23:54:50 +0300

    *

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Tweet cards !!!</header>
+      <header className="App-header">Tweet cards</header>
     </div>
   );
 }
