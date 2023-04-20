01985ba992d260fc1d253348f7db88928cf1c868
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-04-21 00:25:37 +0300

    the assembly is ready

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyle } from '../constants/GlobalStyle';
+
+export const App = () => {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header" style={{ backgroundColor: '#5736A3' }}>
+          Tweet cards !
+        </header>
+      </div>
+      <GlobalStyle />
+    </>
+  );
+};
